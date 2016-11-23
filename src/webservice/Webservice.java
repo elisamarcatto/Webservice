205332a63078810ed14f8449adfca2987fe5322f
Elisa <Elisa@Ultra>
2016-11-23 15:34:54 -0200

    Alteração na classe principal 'Webservice': recebendo ID do usuario como entrada do programa e retornando o contador de vagas

--- src/webservice/Webservice.java
+++ src/webservice/Webservice.java
@@ -6,67 +6,48 @@ package webservice;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Type;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
+import java.util.Scanner;
 
 public class Webservice
 {
      public static void main(String[] args) throws Exception {
-         
-        Gson gson = new Gson();
-
-        String json = getJson("http://ec2-35-164-139-210.us-west-2.compute.amazonaws.com/hirers/222333/opportunities");
-
-        System.out.println("1");
-        System.out.println(json);   
-        //json = "{\"created_at\":\"2016-07-01 13:06:12\",\"updated_at\":\"2016-11-21 15:21:07\",\"category\":{\"id\":1,\"name\":\"Empregada Dom\\u00e9stica\"},\"hirer\":{\"id\":111222,\"name\":\"Felipe Matos Dos Santos\",\"account_type\":\"pf\",\"cnpj\":null,\"company_contact_name\":null,\"phone\":\"(21) 99870-0327\",\"email\":\"felipe_med@yahoo.com.br\",\"mobile_phone\":\"(21) 93234-8378\",\"is_plan_active\":true},\"location\":{\"neighborhood\":\"Ipanema\",\"address\":\"Prudente de Morais\",\"address_type\":\"Rua\",\"latitude\":-22.9851707,\"longitude\":-43.2071601,\"city_id\":\"6861\",\"city\":\"Rio de Janeiro\",\"zipcode\":\"22420043\",\"state\":\"RJ\"},\"id\":98765,\"title\":\"Vaga para Empregada Dom\\u00e9stica\",\"description\":\"Casal sem filhos e sem animais dom\\u00e9sticos  procura empregada \\n            dom\\u00e9stica para cuidar de um apartamento duplex. Limpar, cozinhar, lavar e \\n            passar roupas. In\\u00edcio a partir de 20 de julho\",\"is_contact_available\":true,\"is_active\":true,\"frequency\":\"mensalista_2x\",\"is_automatic\":false,\"score\":3,\"salary_requirements\":1100,\"characteristics\":[],\"starts\":\"esse_mes\",\"amount_candidates\":19,\"amount_visualizations\":58,\"feedback\":\"\",\"salary_research\":\"\",\"relevancy\":\"\"}";
-       
-        //Type listType = new TypeToken<List<Registration>>(){}.getType();
-        //List<Registration> registration = gson.fromJson(json, listType);
-            
-        //JSONObject obj = registration.getJSONObject(1);
+        
+        /* Leitura do teclado para receber ID do usuario */ 
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Insira o numero de ID: ");
+        String id = keyboard.nextLine();
+        String URL = "http://ec2-35-164-139-210.us-west-2.compute.amazonaws.com"+"/hirers/"+id+"/opportunities"; 
+        
+        /* Obtendo JSON a partir da URL */
+        String json = getJson(URL);  
+        
+        Gson gson = new Gson();   
         
         JsonParser parser = new JsonParser();
-        JsonArray jArray = parser.parse(json).getAsJsonArray();
+        JsonArray jArray = parser.parse(json).getAsJsonArray();        
         
-        //JSONArray id = jsonobject.getJSONArray("id");
-        
-        //Registration registration = gson.fromJson(json, Registration.class);
-        //System.out.println(registration.get(0).category.id);
-        System.out.println(jArray.size());
+        System.out.println("Contador de vagas do usuario '"+id+"': "+jArray.size());
     }
 
     private static String getJson(String urlString) throws Exception {
         BufferedReader reader = null;
         try {
             URL url = new URL(urlString);
-            reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            System.out.println(reader);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));           
             String output;
             String jsonString = null;
             
-            while ((output = reader.readLine()) != null) {
-                
-                jsonString = output;
-		System.out.println(output);
+            while ((output = reader.readLine()) != null) {                
+                jsonString = output;		
             } 
             return jsonString;
         } finally {
             if (reader != null)
                 reader.close();
         }
-
     }
 }
 
