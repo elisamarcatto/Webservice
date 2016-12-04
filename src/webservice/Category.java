754e1ac6b9bf31b67c8aa5998c0ccf16ce371295
elisamarcatto <elisamarcatto@gmail.com>
2016-12-04 01:36:07 -0200

    Alteração na classe  'Category': adicionando métodos setters/getters

--- src/webservice/Category.java
+++ src/webservice/Category.java
@@ -5,6 +5,22 @@ package webservice;
 
 class Category
 {
-    Integer id;
-    String name;
+    private String id;
+    private String name;
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public String getId(){
+        return this.id;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
 }
